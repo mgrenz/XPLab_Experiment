7450918225b6de2a4bf484871b26359aeb145e37
mgrenz <mgrenz@uni-osnabrueck.de>
2021-08-05 20:11:42 +0200

    mode change

--- experiments/01_pilot/06_main.js
+++ experiments/01_pilot/06_main.js
@@ -28,7 +28,7 @@ $("document").ready(function() {
             // Possible deployment methods are:
             // "debug" and "directLink
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
+            deployMethod: "directLink",
             contact_email: "mgrenz@uos.de",
             //prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
