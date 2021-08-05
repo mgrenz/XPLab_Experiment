97e5d0a0e2e894ba1e56b05420b033f7b79c39b3
mgrenz <mgrenz@uni-osnabrueck.de>
2021-08-05 20:17:47 +0200

    ID change

--- experiments/01_pilot/06_main.js
+++ experiments/01_pilot/06_main.js
@@ -23,7 +23,7 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "275",
+            experimentID: "277",
             serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink
